74dba3adf93b41f357f22a292bd6a93eec70bd56
Alessio Milanese <milanese.alessio@gmail.com>
2021-03-10 17:31:50 +0100

    print more info

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 # index
-./read_counter index -db test/test.fasta &> /dev/null
-./read_counter map -db test/test.fasta -s test/test.fq -o test/RES_temp &> /dev/null
+echo "RUN INDEX -----------------------------------------------------------"
+./read_counter index -db test/test.fasta
 
+echo "RUN MAP -------------------------------------------------------------"
+./read_counter map -db test/test.fasta -s test/test.fq -o test/RES_temp
+
+echo "RESULT --------------------------------------------------------------"
 cmp --silent test/RES_temp test/correct_result.map && echo 'You have the same result as the test' || echo '### WARNING: Files Are Different! ###'
 
 # remove file created
